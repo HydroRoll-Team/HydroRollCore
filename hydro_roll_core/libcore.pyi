ea840bbc63843bd605a00da31f80241a4f2a9746
简律纯 <i@jyunko.cn>
2024-06-17 00:22:18 +0800

    chore(deps|rust): add clap dependencies

--- hydro_roll_core/libcore.pyi
+++ hydro_roll_core/libcore.pyi
@@ -1,7 +1,7 @@
-class libcore:
+class libcore(object):
     """Core library for hydro roll"""
 
-    def __init__(self): ...
+    def __init__(self, name: str = ""): ...
 
-    def process_rule_pack(rule_pack: str) -> str:
+    def process_rule_pack(self, rule_pack: str) -> str:
         ...
--- tests/test_corelib.py
+++ tests/test_corelib.py
@@ -6,6 +6,9 @@ def main():
     rule_pack = "example_rule_pack"
     result = cb.process_rule_pack(rule_pack)
     print(result)
+    print(cb.name)
+    cb.name = "a"
+    print(cb.name)
 
 if __name__ == "__main__":
     main()
