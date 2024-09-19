bc57c1410c08323ba37114082d0fe609fafc2c5d
HsiangNianian <i@jyunko.cn>
2024-09-19 14:46:35 +0800

    feat(dev): Add model attributes to GRPS class in `grps` folder

--- hrc/dev/grps/v1.py
+++ hrc/dev/grps/v1.py
@@ -1,21 +1,12 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 
 __version__ = "1.0.0-alpha.1"
 
 class GRPS(BaseModel):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-
-    def run(self):
-        pass
-    
-    def start(self):
-        pass
-
-    def stop(self):
-        pass
-
-    def restart(self):
-        pass
+    id: str
+    name: str
+    description: str
+    created_at: datetime
+    updated_at: datetime
